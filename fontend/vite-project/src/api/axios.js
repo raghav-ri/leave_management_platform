0465ef6a29764ac6a144d0d097923887525735fe
Raghav Issar <raghavissa701@gmail.com>
2026-03-07 18:19:13 +0530

    Update API base URL to use environment variable

--- fontend/vite-project/src/api/axios.js
+++ fontend/vite-project/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:  "https://leave-management-platform.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
